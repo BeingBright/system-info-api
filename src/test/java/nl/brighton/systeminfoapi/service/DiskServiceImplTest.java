16e07aaef1f7641d4124bd47c63f1d0953cadfbe
BeingBright <BrightonvandenEnd@gmail.com>
2019-12-22 22:43:58 +0100

    added test templates

---
+++ src/test/java/nl/brighton/systeminfoapi/service/DiskServiceImplTest.java
@@ -0,0 +1,12 @@
+package nl.brighton.systeminfoapi.service;
+
+import org.junit.jupiter.api.BeforeEach;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class DiskServiceImplTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+}
